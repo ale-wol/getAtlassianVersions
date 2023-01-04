702bcec66803a7263c63729c0b8397e999c04cfc
Alexander Wolf <alexander_wolf91@web.de>
2023-01-04 22:36:11 +0100

    refactoring

---
+++ atlasUtil.py
@@ -0,0 +1,53 @@
+import re
+import json
+import datetime
+
+##########################################################################
+#### Methods
+##########################################################################
+
+def loadsJsonp(_jsonp):
+    try:
+         jsonRaw = re.match(".*?\(\[({.*})]\)",_jsonp,re.S).group(1)
+         wrappedJson  = ''.join(('[',jsonRaw,']'))
+         return json.loads(wrappedJson)
+    except:
+        raise ValueError('Invalid Input')
+
+def convertListDate(json_list, format):
+    for i in json_list:
+        #print(datetime.datetime.strptime(i['released'], '%d-%b-%Y').strftime('%Y-%m-%d'))
+        i['released'] = datetime.datetime.strptime(i['released'], format).strftime('%Y-%m-%d')
+    return json_list
+
+def findLatestRelease(json_list):
+    youngestVersion = None
+    youngestVersionReleased = datetime.time(0,0,0)
+    for i in json_list:
+        if(youngestVersion is None):
+            youngestVersion=i['version']
+            youngestVersionReleased=i['released']
+        elif(youngestVersionReleased < i['released']):
+            youngestVersionReleased=i['released']
+            youngestVersion=i['version']
+    return youngestVersion
+
+def findLatestLongTermRelease(json_list, longTermRelease):
+    lts_json_list=list()
+    youngestLtsVersion=None
+    youngestLtsVersionReleased = datetime.time(0,0,0)
+    for i in json_list:
+        versionString=str(i['version'])
+        if(versionString.startswith(str(longTermRelease))):
+            lts_json_list.append(i)
+    
+    convertListDate(lts_json_list, '%Y-%m-%d')
+    for i in lts_json_list:
+        if(youngestLtsVersion is None):
+            youngestLtsVersion=i['version']
+            youngestLtsVersionReleased=i['released']
+        elif(youngestLtsVersionReleased < i['released']):
+            youngestLtsVersionReleased=i['released']
+            youngestLtsVersion=i['version']
+    
+    return youngestLtsVersion
--- getConfluenceVersion.py
+++ getConfluenceVersion.py
@@ -1,8 +1,8 @@
 from urllib.request import urlopen
-import datetime
-import json
-import re
-import json
+from atlasUtil import loadsJsonp
+from atlasUtil import convertListDate
+from atlasUtil import findLatestRelease
+from atlasUtil import findLatestLongTermRelease
 
 #Current Versions Confluence
 #https://my.atlassian.com/download/feeds/current/confluence.json
@@ -17,66 +17,13 @@ import json
 #### Properties
 ##########################################################################
 longTermRelease=7
-url="https://my.atlassian.com/download/feeds/current/confluence.json"
-
-##########################################################################
-#### Methods
-##########################################################################
-
-def loadsJsonp(_jsonp):
-    try:
-         jsonRaw = re.match(".*?\(\[({.*})]\)",_jsonp,re.S).group(1)
-         wrappedJson  = ''.join(('[',jsonRaw,']'))
-         return json.loads(wrappedJson)
-    except:
-        raise ValueError('Invalid Input')
-
-#Iterate through json list
-# create new list with newest versions...
-#Convert Datetime String to Datetime
-def convertListDate(json_list, format):
-    for i in json_list:
-        #print(datetime.datetime.strptime(i['released'], '%d-%b-%Y').strftime('%Y-%m-%d'))
-        i['released'] = datetime.datetime.strptime(i['released'], format).strftime('%Y-%m-%d')
-    return json_list
-
-def findLatestRelease(json_list):
-    youngestVersion = None
-    youngestVersionReleased = datetime.time(0,0,0)
-    for i in json_list:
-        if(youngestVersion is None):
-            youngestVersion=i['version']
-            youngestVersionReleased=i['released']
-        elif(youngestVersionReleased < i['released']):
-            youngestVersionReleased=i['released']
-            youngestVersion=i['version']
-    return youngestVersion
-
-def findLatestLongTermRelease(json_list):
-    lts_json_list=list()
-    youngestLtsVersion=None
-    youngestLtsVersionReleased = datetime.time(0,0,0)
-    for i in json_list:
-        versionString=str(i['version'])
-        if(versionString.startswith(str(longTermRelease))):
-            lts_json_list.append(i)
-    
-    convertListDate(lts_json_list, '%Y-%m-%d')
-    for i in lts_json_list:
-        if(youngestLtsVersion is None):
-            youngestLtsVersion=i['version']
-            youngestLtsVersionReleased=i['released']
-        elif(youngestLtsVersionReleased < i['released']):
-            youngestLtsVersionReleased=i['released']
-            youngestLtsVersion=i['version']
-    
-    return youngestLtsVersion
+jsonurl="https://my.atlassian.com/download/feeds/current/confluence.json"
 
 ##########################################################################
 #### Script Run
 ##########################################################################
 
-response=urlopen(url)
+response=urlopen(jsonurl)
 
 json_str=response.read().decode('utf-8')
 
@@ -86,5 +33,5 @@ convertListDate(json_list, '%d-%b-%Y')
 
 print('#################################################################################')
 print("Latest Release: " + findLatestRelease(json_list))
-print("Latest LTS: " + findLatestLongTermRelease(json_list))
+print("Latest LTS: " + findLatestLongTermRelease(json_list, longTermRelease))
 print('#################################################################################')
